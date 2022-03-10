0087aa8aec9d579248b5019dfb581bf861be1b91
Michael Chilaka <obichilaka@gmail.com>
2022-03-10 08:16:08 +0100

    set variable states for user & messages

--- src/components/chats/Chats.jsx
+++ src/components/chats/Chats.jsx
@@ -1,21 +1,29 @@
 import {
   Container,
   Divider,
+  FormControl,
   Grid,
+  IconButton,
   List,
   ListItem,
   ListItemText,
   Paper,
+  TextField,
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
 import { ChatMessageDto } from "../../model/ChatMessageDto";
+import SendIcon from "@mui/icons-material/Send";
+
+import "./Chats.css";
 
 const Chats = () => {
   const [chatMessages, setChatMessages] = useState([
-      new ChatMessageDto("John", "Hi")
+    new ChatMessageDto("John", "Hi"),
   ]);
+  const [user, setUser] = useState("");
+  const [message, setMessage] = useState("");
   const listChatMessages = chatMessages.map((chatMessageDto, index) => (
     <ListItem key={index}>
       <ListItemText
@@ -33,14 +41,28 @@ const Chats = () => {
             </Typography>
             <Divider />
             <Grid container spacing={4} alignItems="center">
-              <Grid id="chat__window" item>
-                <List id="chat__window__message">
-                    {listChatMessages}
-                </List>
+              <Grid id="chat__window" xs={12} item>
+                <List id="chat__window__message">{listChatMessages}</List>
               </Grid>
-              <Grid item></Grid>
-              <Grid item></Grid>
-              <Grid item></Grid>
+              <Grid xs={2} item>
+                <FormControl fullWidth>
+                  <TextField value={user} label="Nickname" variant="outlined" />
+                </FormControl>
+              </Grid>
+              <Grid xs={9} item>
+                <FormControl fullWidth>
+                  <TextField
+                    value={message}
+                    label="Nickname"
+                    variant="outlined"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid xs={1} item>
+                <IconButton>
+                  <SendIcon />
+                </IconButton>
+              </Grid>
             </Grid>
           </Box>
         </Paper>
